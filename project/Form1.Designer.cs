42c2efea16d18728d693ce3840c51cb0f2013bb4
Lord Mirai (未来) <pilotcraft1001@yahoo.com>
2023-03-22 13:40:43 +0200

    tryna fix the damn offsets

--- project/Form1.Designer.cs
+++ project/Form1.Designer.cs
@@ -29,17 +29,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bkgr = new System.Windows.Forms.PictureBox();
+            this.hint = new System.Windows.Forms.Label();
+            this.assistance = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bkgr)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bkgr
+            // 
+            this.bkgr.Image = ((System.Drawing.Image)(resources.GetObject("bkgr.Image")));
+            this.bkgr.InitialImage = ((System.Drawing.Image)(resources.GetObject("bkgr.InitialImage")));
+            this.bkgr.Location = new System.Drawing.Point(0, 0);
+            this.bkgr.Name = "bkgr";
+            this.bkgr.Size = new System.Drawing.Size(604, 540);
+            this.bkgr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bkgr.TabIndex = 0;
+            this.bkgr.TabStop = false;
+            this.bkgr.Click += new System.EventHandler(this.bkgr_Click);
+            // 
+            // hint
+            // 
+            this.hint.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint.Location = new System.Drawing.Point(12, 683);
+            this.hint.Name = "hint";
+            this.hint.Size = new System.Drawing.Size(766, 28);
+            this.hint.TabIndex = 1;
+            this.hint.Text = "Hint:";
+            this.hint.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // assistance
+            // 
+            this.assistance.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.assistance.Location = new System.Drawing.Point(342, 130);
+            this.assistance.Name = "assistance";
+            this.assistance.Size = new System.Drawing.Size(5, 5);
+            this.assistance.TabIndex = 2;
+            this.assistance.Text = "X";
+            this.assistance.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(829, 720);
+            this.Controls.Add(this.assistance);
+            this.Controls.Add(this.hint);
+            this.Controls.Add(this.bkgr);
             this.Name = "Form1";
             this.Text = "Wildebeest Schach";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bkgr)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label assistance;
+
+        private System.Windows.Forms.Label hint;
+
+        private System.Windows.Forms.PictureBox bkgr;
 
         #endregion
     }
--- project/Form1.cs
+++ project/Form1.cs
@@ -1,12 +1,86 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace project
 {
     public partial class Form1 : Form
     {
+        static int offsetX = 15, offsetY = 15;
+        static int offsetEndX = 625, offsetEndY = 705;
+        static int cellSize = 51;
+
+        private int[,] matrix = new int[10, 11];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void bkgr_Click(object sender, EventArgs e)
+        {
+            int x, y, startX, startY;
+
+            startX = Location.X;
+            startY = Location.Y;
+
+            x = MousePosition.X - startX - offsetX;
+            y = MousePosition.Y - startY - offsetY;
+
+            int[] pos = getPosition();
+            hint.Text = String.Format("Pos: [{0}, {1}] {2}, {3} ({4})", x, y, pos[0], pos[1], getPositionFormatted());
+        }
+
+
+        public int[] getPosition()
+        {
+            int offsetX = 105, offsetY = 125;
+            int offsetEndX = 560, offsetEndY = 532;
+
+            int[] position = { -1, -1 };
+
+
+            if (!(offsetX < MousePosition.X && MousePosition.X < offsetEndX) ||
+                !(offsetY < MousePosition.Y && MousePosition.Y < offsetEndY))
+            {
+                // invalid position (out of bounds)
+                return position;
+            }
+
+            int x = MousePosition.X - offsetX;
+            int y = MousePosition.Y - offsetY;
+
+            x = x / cellSize;
+            y = y / cellSize;
+
+            position[0] = x;
+            position[1] = y;
+            return position;
+        }
+
+        public string getPositionFormatted()
+        {
+            string outS = "";
+            int[] pos = getPosition();
+            if (pos[0] != -1)
+            {
+                char let = (char)(65 + pos[0]);
+                outS = String.Format("{0}{1}", let, pos[1] + 1);
+            }
+
+            return outS;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            assistance.Location = new System.Drawing.Point(offsetX, offsetY);
+            
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    matrix[i, j] = 0; // init empty matrix
+                }
+            }
         }
     }
 }
